d8aef17acee201638a7cc20fbafaa7b84aa08c3e
Leonardo dos Santos Bispo <leonardosantosbispo@gmail.com>
2020-06-04 06:50:03 -0300

    Aula 4 finalizada

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/ServletSpringMvc.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/ServletSpringMvc.java
@@ -1,5 +1,8 @@
 package br.com.casadocodigo.loja.conf;
 
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class ServletSpringMvc extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -12,12 +15,21 @@ public class ServletSpringMvc extends AbstractAnnotationConfigDispatcherServletI
 
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
-		  return new Class[] {AppWebConfiguration.class,JPAConfiguration.class};
+		return new Class[] { AppWebConfiguration.class, JPAConfiguration.class };
 	}
 
 	@Override
 	protected String[] getServletMappings() {
-		  return new String[] {"/"};
+		return new String[] { "/" };
 	}
+	
+	//Metodo que aceita requisição com caracteres especiais
+
+	@Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+        encodingFilter.setEncoding("UTF-8");
+        return new Filter[] {encodingFilter};
+    }
 
 }
---
+++ casadocodigo/src/main/webapp/WEB-INF/views/produtos/lista.jsp
@@ -0,0 +1,37 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+	<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Livros de Java, Android, iPhone, Ruby, PHP e muito mais -
+	Casa do Código</title>
+</head>
+<body>
+
+	<h1>Lista de Produtos</h1>
+	<table>
+
+		<tr>
+			<td>Título</td>
+			<td>Descrição</td>
+			<td>Páginas</td>
+		</tr>
+
+		<c:forEach items="${produtos}" var="produto">
+			<tr>
+				<td>${produto.titulo}</td>
+				<td>${produto.descricao}</td>
+				<td>${produto.paginas}</td>
+			</tr>
+
+
+		</c:forEach>
+
+
+	</table>
+
+
+</body>
+</html>
